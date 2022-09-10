2f06468630de49534ce352e005738bab02289c34
Photon94 <299792458.photon.94@gmail.com>
2022-09-10 12:41:47 +1000

    Added pid control

--- msur_driver/msur_driver/driver.py
+++ msur_driver/msur_driver/driver.py
@@ -3,7 +3,8 @@ from rclpy.node import Node
 from geometry_msgs.msg import Twist
 from std_msgs.msg import Header
 from msur_packages.driver.service import Client
-from msur_packages.driver.protocol import XThrust, YThrust, ZThrust, Yaw
+from msur_packages.driver.protocol import XThrust, YThrust, ZThrust, Yaw, PidStats
+from msur_msgs.msg import PidStatus
 
 
 class Driver(Node):
@@ -17,12 +18,18 @@ class Driver(Node):
     def __init__(self):
         super().__init__(self.header.frame_id)
         self.subscription = self.create_subscription(Twist, 'cmd_vel', self.driver_callback, 10)
+        self.pid_subscription = self.create_subscription(Twist, 'control/pid', self.pid_callback, 10)
         self.client = Client()
         self.get_logger().info('Узел драйвера запущен!')
 
     def driver_callback(self, msg: Twist):
         self.get_logger().info(f"{msg}")
         self.client.send([XThrust(value=msg.linear.x), YThrust(value=msg.linear.y), ZThrust(value=msg.linear.z), Yaw(value=msg.angular.z)])
+
+
+    def pid_callback(self, msg: PidStatus):
+        self.get_logger().info(f"{msg}")
+        self.client.send([PidStats(yaw=msg.yaw, pitch=msg.pitch, roll=msg.roll), ])
 
 
 def main(args=None):
